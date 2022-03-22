95074fcda636674b3a749349dbb3cf8f8a5a56f1
Joram van den Boezem <joram@vandenboezem.nl>
2022-03-22 15:01:41 +0000

    test: expand type tests

--- test-d/command.test-d.ts
+++ test-d/command.test-d.ts
@@ -4,3 +4,20 @@ import { Command, command } from '../src/command'
 const cmd = command()
 
 expectType<Command>(cmd)
+
+// Chainable methods
+expectType<Command>(cmd.description('foo'))
+expectType<Command>(cmd.hidden())
+expectType<Command>(cmd.add(cmd))
+expectType<Command>(cmd.default())
+expectType<Command>(cmd.action(undefined))
+
+// Argument types
+cmd.argument('foo').action((args) => {
+  expectType<{ foo: string }>(args)
+})
+
+// Option types
+cmd.option('foo').action((args) => {
+  expectType<{ foo: unknown }>(args)
+})
